6c77fb61ca6e469df2dba59e0ee19f048b6c513d
CGill98 <coopergill4@gmail.com>
2020-11-18 12:52:47 +0000

    Working on sql

--- cms/scripts/form-dom.js
+++ cms/scripts/form-dom.js
@@ -1,24 +1,51 @@
-let examples = $('#examples-div');
+//catergory is either: examples or keypoints
+const changedCatergoryDivs = (catergory) => {
+    const cat_inputs = catergory.children('div').children('input')
 
-const ex_inputs = examples.children('div').children('input')
+    let cat_input_id_counter = 1;
 
-let ex_input_id_counter = 1;
-
-function addInput(event) {
-    const examples = event.data.div
-    if (event.key === 'Enter') {
-        let div = $(`<div id='ex-input-${ex_input_id_counter}'>
-                        <input id='${ex_input_id_counter++}' type="text"/>
-                    </div>`)
-        div.on('keydown', addInput)
-        examples.append(div)
+    function addInput(event) {
+        //const examples = event.data.div
+        if (event.key === 'Enter') {
+            let div = $(`<div id='ex-input-${cat_input_id_counter}'>
+                            <input id='${cat_input_id_counter++}' type="text"/>
+                        </div>`)
+            div.on('keydown', addInput)
+            catergory.append(div)
+        }
+        else if (event.key === 'Backspace' && event.target.value === "") {
+            catergory.children(`#ex-input-${event.target.id}`).remove()
+        }
     }
-    else if (event.key === 'Backspace' && event.target.value === "") {
-        examples.children(`#ex-input-${event.target.id}`).remove()
-    }
+
+    cat_inputs.on('keydown', addInput)
 }
 
-ex_inputs.on('keydown', {div: examples} , addInput)
+changedCatergoryDivs($('#examples-div'));
+changedCatergoryDivs($('#key-points-div'));
+
+const getValsFromInputs = (examples) => { 
+    let ex_strings = []
+    for (let i = 0; i < examples.length; i++) {
+        console.log($(examples[i]).val())
+        ex_strings.push($(examples[i]).val())
+    }
+    return ex_strings; 
+}
+
+
+$('#form-btn').on('click', ()=>{ 
+    const form_textarea = $('#form-textarea').val()
+
+    const examples = $('#examples-div').children('div').children('input')
+    const examples_string = getValsFromInputs(examples);
+    const key_points = $('#key-points-div').children('div').children('input')
+    const key_points_string = getValsFromInputs(key_points);
+
+    const sql_string = form_textarea + examples_string + key_points_string;
+    $('#output-text-div').text(sql_string)
+})
+
 
 window.onload = () => console.log("loaded")
 
